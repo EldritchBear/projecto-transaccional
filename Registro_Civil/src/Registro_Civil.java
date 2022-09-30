00b5fb141e7875fd7bfd214aa5a1f5a3135d1bf7
lekkice <lekkice0@gmail.com>
2022-09-30 11:53:55 -0300

    escritura de csv al cerrar ventana

--- Registro_Civil/src/Registro_Civil.java
+++ Registro_Civil/src/Registro_Civil.java
@@ -1,23 +1,38 @@
 import javax.swing.*;
+import java.awt.event.*;
+class WindowEventHandler extends WindowAdapter {
+	Regiones regiones;
+	public WindowEventHandler(Regiones regiones) {
+		this.regiones = regiones;
+	}
+	public void windowClosing(WindowEvent evt) {
+		try {
+			regiones.escribirTxt("registro.csv");
+		} catch(Exception e) {
+			System.out.println("No se pudo escribir en registro.csv");
+		}
+	}
+}
 
 public class Registro_Civil {
 	
 	public static void main(String[] args) {
 		Regiones regiones = new Regiones();
 		try {
-			regiones.leerTxt("registro.txt");
+			regiones.leerTxt("registro.csv");
 		} catch (Exception e) {
 			System.out.println("No se pudo leer el archivo registro.txt");
 		}
-		JFrame frame = inicializarVentana();
+		JFrame frame = inicializarVentana(regiones);
 		new VentanaMenu(regiones, frame);
 		frame.setSize(512, 512);
 	}
 
-	public static JFrame inicializarVentana() {
+	public static JFrame inicializarVentana(Regiones regiones) {
 		JFrame frame = new JFrame("Menú");
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.addWindowListener(new WindowEventHandler(regiones));
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		return frame;
 	}
 }
