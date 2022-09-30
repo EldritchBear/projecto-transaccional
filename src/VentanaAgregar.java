8edaedd2f83f3c0fedd4ff42ff4874193d679847
Lekkice <lekkice0@gmail.com>
2022-09-30 16:36:17 -0300

    problemas sugeridos por el IDE solucionados

--- src/VentanaAgregar.java
+++ src/VentanaAgregar.java
@@ -1,6 +1,4 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 // falta implementar checkboxes para crear Personas tipo Extranjero y Funcionario
 // se podría usar una sobrecarga para crearPersona(), una que reciba un bool y otra que reciba un String con la función
@@ -22,60 +20,38 @@ public class VentanaAgregar {
     private JCheckBox extranjeroCheckBox;
     private JTextField funcionT;
 
-    private Regiones regiones;
-    private JFrame ventana;
     public VentanaAgregar(Regiones regiones, JFrame ventana) {
-        this.regiones = regiones;
-        this.ventana = ventana;
         funcionT.setEnabled(false);
-        cancelarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ventana.setContentPane(new VentanaMenu(regiones, ventana).panel);
-                ventana.setSize(512, 512);
-            }
+        cancelarButton.addActionListener(e -> {
+            ventana.setContentPane(new VentanaMenu(regiones, ventana).panel);
+            ventana.setSize(512, 512);
         });
-        agregarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int reg = Integer.parseInt(regionT.getText());
-                String nom = nombreT.getText();
-                String r = rutT.getText();
-                int ed = Integer.parseInt(edadT.getText());
+        agregarButton.addActionListener(e -> {
+            int reg = Integer.parseInt(regionT.getText());
+            String nom = nombreT.getText();
+            String r = rutT.getText();
+            int ed = Integer.parseInt(edadT.getText());
 
-                try {
-                    regiones.getRegion(reg).agregarPersona(nom, r, ed);
-                } catch(Exception ex) {
-                    JOptionPane.showMessageDialog(ventana, ex.getMessage());
-                    return;
-                }
+            try {
+                regiones.getRegion(reg).agregarPersona(nom, r, ed);
+            } catch(Exception ex) {
+                JOptionPane.showMessageDialog(ventana, ex.getMessage());
+                return;
+            }
 
 
-                ventana.setContentPane(new VentanaMenu(regiones, ventana).panel);
-                ventana.setSize(512, 512);
+            ventana.setContentPane(new VentanaMenu(regiones, ventana).panel);
+            ventana.setSize(512, 512);
+        });
+        funcionarioCheckBox.addActionListener(e -> {
+            if (funcionarioCheckBox.isSelected()) {
+                extranjeroCheckBox.setEnabled(false);
+                funcionT.setEnabled(true);
+            } else {
+                extranjeroCheckBox.setEnabled(true);
+                funcionT.setEnabled(false);
             }
         });
-        funcionarioCheckBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (funcionarioCheckBox.isSelected()) {
-                    extranjeroCheckBox.setEnabled(false);
-                    funcionT.setEnabled(true);
-                } else {
-                    extranjeroCheckBox.setEnabled(true);
-                    funcionT.setEnabled(false);
-                }
-            }
-        });
-        extranjeroCheckBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (extranjeroCheckBox.isSelected()) {
-                    funcionarioCheckBox.setEnabled(false);
-                } else {
-                    funcionarioCheckBox.setEnabled(true);
-                }
-            }
-        });
+        extranjeroCheckBox.addActionListener(e -> funcionarioCheckBox.setEnabled(!extranjeroCheckBox.isSelected()));
     }
 }
