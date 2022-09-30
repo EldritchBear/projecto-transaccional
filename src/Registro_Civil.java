e49e3c466c7688a3e5ec76c54e1c0d464499627e
Lekkice <lekkice0@gmail.com>
2022-09-30 16:29:34 -0300

    carpetas reordenadas y cambios a la lista para poder filtrar por región más fácilmente

---
+++ src/Registro_Civil.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.*;
+class WindowEventHandler extends WindowAdapter {
+	Regiones regiones;
+	public WindowEventHandler(Regiones regiones) {
+		this.regiones = regiones;
+	}
+	public void windowClosing(WindowEvent evt) {
+		try {
+			regiones.escribirTxt("registro.csv");
+		} catch(Exception e) {
+			System.out.println("No se pudo escribir en registro.csv");
+			System.out.println(e.getMessage());
+		}
+		System.exit(1);
+	}
+}
+
+public class Registro_Civil {
+	
+	public static void main(String[] args) {
+		Regiones regiones = new Regiones();
+		try {
+			regiones.leerTxt("registro.csv");
+		} catch (Exception e) {
+			System.out.println("No se pudo leer el archivo registro.txt");
+		}
+		JFrame frame = inicializarVentana(regiones);
+		new VentanaMenu(regiones, frame);
+		frame.setSize(512, 512);
+	}
+
+	public static JFrame inicializarVentana(Regiones regiones) {
+		JFrame frame = new JFrame("Menú");
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowEventHandler(regiones));
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		return frame;
+	}
+}
--- src/VentanaLista.java
+++ src/VentanaLista.java
@@ -18,25 +18,37 @@ public class VentanaLista {
 
     private Region region = null;
 
-    public VentanaLista(Regiones regiones, JFrame frame) {
-        this.regiones = regiones;
-        this.frame = frame;
-
+    public void refresh() {
         ArrayList<Persona> arreglo;
         String[] column = {"Región", "Nombre", "Rut", "Edad"};
         DefaultTableModel tableModel = new DefaultTableModel(column, 0);
-
-        table.setModel(tableModel);
         table.setDefaultEditor(Object.class, null);
 
-        for (int i = 1; i < 16; i++) {
-            try {arreglo = regiones.getRegion(i).getArray();} catch(Exception e) {continue;}
-            if (arreglo.size() == 0) continue;
-            for (Persona persona : arreglo) {
+        if (region != null) {
+            for (Persona persona : region.getArray()) {
                 tableModel.addRow(persona.getObjs());
             }
+        } else {
+            for (int i = 1; i < 16; i++) {
+                try {
+                    arreglo = regiones.getRegion(i).getArray();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                }
+                if (arreglo.size() == 0) continue;
+                for (Persona persona : arreglo) {
+                    tableModel.addRow(persona.getObjs());
+                }
+            }
         }
+        table.setModel(tableModel);
+    }
 
+    public VentanaLista(Regiones regiones, JFrame frame) {
+        this.regiones = regiones;
+        this.frame = frame;
+        this.refresh();
         volverButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -71,12 +83,14 @@ public class VentanaLista {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String filtro = filtroT.getText();
+                System.out.println(filtro);
                 ArrayList<Persona> personas;
                 if (region == null) {
                     personas = regiones.getPersonas();
                 } else {
                     personas = region.getArray();
                 }
+                String[] column = {"Región", "Nombre", "Rut", "Edad"};
                 DefaultTableModel nuevoModel = new DefaultTableModel(column, 0);
                 for (Persona persona : personas) {
                     if (filtro.length() == 0 || persona.getRegion().getNombre().contains(filtro) || persona.getNombre().contains(filtro) ||
@@ -91,14 +105,14 @@ public class VentanaLista {
         regionSeleccionada.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String stringSeleccion = (String)regionSeleccionada.getSelectedItem();
-                stringSeleccion = String.valueOf(stringSeleccion.charAt(0));
+                String stringSeleccion = ((String)regionSeleccionada.getSelectedItem()).substring(0, 2);
                 try {
-                    region = regiones.getRegion(Integer.parseInt(stringSeleccion));
+                    int i = Integer.parseInt(stringSeleccion.replaceAll("\\.", "")); // elimina punto
+                    region = regiones.getRegion(i);
                 } catch (Exception exc) {
-                    System.out.println("Región no encontrada");
                     region = null;
                 }
+                refresh();
             }
         });
     }
